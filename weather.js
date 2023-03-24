d6f1df85b0d6ec15cb22eb2e60ba5471c4a800a4
vitorhugo1207 <vitorhugoalvesferreira@gmail.com>
2023-03-24 17:17:23 -0300

    get weather is ready

---
+++ weather.js
@@ -0,0 +1,45 @@
+const { rejects } = require('assert');
+
+module.exports = class Weather{
+    constructor(){
+        this.url = "https://api.weatherapi.com/v1/current.json?";
+        this.apikey = this.getApiKey();
+        this.city = 'Getulina'; // Forward it's call a function to get the geolocalization
+        this.weather = undefined;
+    }
+
+    getApiKey(){
+        const fs = require('fs')
+        const path = require('path')
+        this.apikey = fs.readFileSync(__dirname + '/APIKEY.json', 'utf-8', (err, data) => {
+            if (err){
+                return;
+            }
+            this.apikey = data;
+        })
+        this.apikey = JSON.parse(this.apikey); // transform reponse in a json object
+        return this.apikey = this.apikey.key;
+    }
+
+    requestWeather(){
+        return new Promise((resolve, reject) => { // callback function, resolve is a function will return my value, reject is a function will return error. This function is async then when for call it use async/await.
+            const request = require('https');
+
+            request.get(`${this.url}key=${this.apikey}&q=${this.city}&days=3&aqi=yes&alerts=yes`, (resp) => {
+                let data = '';
+
+                // Chunk of data has been received.
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                })
+
+                // The whole response has been received. Print out the result. 
+                resp.on('end', () => {
+                    resolve(JSON.parse(data));
+                })
+            }).on("error", (err) => {
+                reject(err);
+            });
+        })
+    }
+}
