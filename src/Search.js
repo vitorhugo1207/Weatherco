1e06037ec6eedff4c5cffa9a30dfd68734b47fc0
vitorhugo1207 <vitorhugoalvesferreira@gmail.com>
2023-07-25 12:14:10 -0300

    Trying to use react router

---
+++ src/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Search.css";
+import { Weather } from "./weather";
+
+function Search(){
+    return(
+        <div>
+            <p>This is Search page!!!</p>
+        </div>
+    )
+}
+
+export default Search;
