56a7c8cc2691630dd4efe21ea19897afac48ac04
vitorhugo1207 <vitorhugoalvesferreira@gmail.com>
2023-07-27 15:42:24 -0300

    added input form

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -4,12 +4,35 @@ import "../css/Search.css";
 import { Weather } from "../API/weather";
 
 const Search = () =>{
-    get
+    const [city, setCity] = useState('');
+
+    async function getCity(){
+        const weather = new Weather(city);
+        const response = await weather.getCity();
+        setCity(response);
+    }
+
+    function submit(e){
+        e.preventDefault();
+
+        const form = e.target;
+        const formData = new FormData(form);
+
+        const formJson = Object.fromEntries(formData.entries());
+
+        console.log(formJson.myInput);
+    }
 
     return(
         <div>
-            <p>This is Search page!!!</p>
             <Link to='/'>Home</Link>
+
+            <form method='post' onSubmit={submit}>
+                <label>
+                    <input name='myInput' />
+                </label>
+                <button type='submit'>Submit</button>
+            </form>
         </div>
     )
 }
