e0a52087759e3d8f63aefcfafd3aaec34ff6a934
vitorhugo1207 <vitorhugoalvesferreira@gmail.com>
2023-08-03 17:56:40 -0300

    Added react-select

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,64 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { Link, useNavigate } from 'react-router-dom'
 import "../css/Search.css";
 import { Weather } from "../API/weather";
+import Select from 'react-select';
+
 
 // todo Make a style to Search box
 // todo Make a autocomplite (https://react.dev/reference/react-dom/components/input)
 // todo Make a button arrow style
 // todo Make a enter press key event
 
-const Selection = ({ cityResp }) => {
-    const [citiesElements, setCitiesElements] = useState([]);
-    const navegate = useNavigate();
-    
-    function setSelection(){
-        const newOption = [];
-
-        for(let x = 0; x < cityResp.length; x++){
-            newOption.push(React.createElement("option", {value:`${cityResp[x]?.name}, ${cityResp[x]?.region}`}, `${cityResp[x]?.name}, ${cityResp[x]?.region}`));
-        }
-        setCitiesElements(newOption);
-    }
-
-    useEffect(() => {
-        setSelection();
-    }, [cityResp])
-
-    const navegateToHome = (e) => {
-        const form = e.target.value;
-        navegate('/', {state:{cityFromSearch: form}});
-    }
-
-    return(
-        <select name="citiesElementsSelector" onClick={(e) => {navegateToHome(e)}}>
-            {citiesElements}
-        </select>
-    )
-}
-
 const Search = () =>{
     const [cityResp, setCityResp] = useState('');
+    const [search, setSearch] = useState([]);
+    const navegate = useNavigate();
 
-    async function getCityResp(e){
-        const weather = new Weather(e.target.value);
-        const response = await weather.getCity();
-        setCityResp(response);
+    const getCityResp = (inputValue) => {
+        const weather = new Weather(inputValue);
+        const response = weather.getCity();
+        response.then(response => {
+            const newOption = [];
+            for(let x = 0; x < response.length; x++){
+                newOption.push({value: `${response[x]?.name}-${response[x]?.region}`, label:`${response[x]?.name}, ${response[x]?.region}`});
+            }
+            setSearch(newOption);
+        });
+    }
+
+    const navegateToHome = (inputValue) => {
+        const value = inputValue.value;
+        navegate('/', {state:{cityFromSearch: value}});
     }
 
     return(
         <div>
             <Link to='/'>Home</Link>
 
-            <label>
-                <input
-                    onChange={(e) => {
-                        setTimeout(() => getCityResp(e), 1000);
-                    }}
-                    autoFocus={true}
-                />
-                <Selection cityResp={cityResp}/>
-            </label>
+            <Select
+                placeholder='Type the name city'
+                options={search}
+                onInputChange={getCityResp}
+                autoFocus={true}
+                onChange={navegateToHome}
+                classNamePrefix="react-select"
+            />
         </div>
     )
 }
