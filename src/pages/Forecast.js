028a9a35dceccf96aa60108a51390c81d5b180ff
vitorhugo1207 <vitorhugoalvesferreira@gmail.com>
2023-08-14 14:57:49 -0300

    Added Forecast files

---
+++ src/pages/Forecast.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../css/forecast.css';
+import '../API/weather';
+
+export default function Forecast({ weatherdata }){
+    return(
+        <>
+            <p>FUNFOU!!!!</p>
+        </>
+    )
+}
