ffbd2259ecdcae55ab02e40f35470e0aef374870
vitorhugo1207 <vitorhugoalvesferreira@gmail.com>
2023-08-15 12:26:12 -0300

    Added Arrow Component

---
+++ src/pages/Arrow.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../css/Arrow.css';
+
+export default function ArrowRight(){
+    return(
+        <>
+            <div class="arrowRight"></div>
+        </>
+    )
+}
+
+export const ArrowLeft = () => {
+    return(
+        <>
+            <div class="arrowLeft"></div>            
+        </>
+    )
+}
